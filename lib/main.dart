b7727cc97b7744c14b1d283b3b845928abfccb88
Pi-Dev-0 <pimathematics1@gmail.com>
2024-12-17 11:06:04 +0600

    Main Page route Fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -102,21 +102,13 @@ import 'pages/class7/english_page.dart';
 import 'pages/class7/math_page.dart';
 import 'pages/class7/science_page.dart';
 import 'pages/class7/social_science_page.dart';
-import 'pages/class7/religion_page.dart';
-import 'pages/class7/ict_page.dart';
-import 'pages/class7/health_page.dart';
-import 'pages/class7/life_career_page.dart';
-import 'pages/class7/arts_culture_page.dart';
+import 'pages/class7/islamic_studies_page.dart';
 import 'pages/class8/bangla_page.dart';
 import 'pages/class8/english_page.dart';
 import 'pages/class8/math_page.dart';
 import 'pages/class8/science_page.dart';
 import 'pages/class8/social_science_page.dart';
-import 'pages/class8/religion_page.dart';
-import 'pages/class8/ict_page.dart';
-import 'pages/class8/health_page.dart';
-import 'pages/class8/life_career_page.dart';
-import 'pages/class8/arts_culture_page.dart';
+import 'pages/class8/islamic_studies_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -265,11 +257,7 @@ class MyApp extends StatelessWidget {
         '/class7/math': (context) => const Class7MathPage(),
         '/class7/science': (context) => const Class7SciencePage(),
         '/class7/social_science': (context) => const Class7SocialSciencePage(),
-        '/class7/religion': (context) => const Class7ReligionPage(),
-        '/class7/ict': (context) => const Class7IctPage(),
-        '/class7/health': (context) => const Class7HealthPage(),
-        '/class7/life_career': (context) => const Class7LifeCareerPage(),
-        '/class7/arts_culture': (context) => const Class7ArtsCulturePage(),
+        '/class7/religion': (context) => const Class7IslamicStudiesPage(),
 
         // Class 8 Routes
         '/class8/bangla': (context) => const Class8BanglaPage(),
@@ -277,11 +265,7 @@ class MyApp extends StatelessWidget {
         '/class8/math': (context) => const Class8MathPage(),
         '/class8/science': (context) => const Class8SciencePage(),
         '/class8/social_science': (context) => const Class8SocialSciencePage(),
-        '/class8/religion': (context) => const Class8ReligionPage(),
-        '/class8/ict': (context) => const Class8IctPage(),
-        '/class8/health': (context) => const Class8HealthPage(),
-        '/class8/life_career': (context) => const Class8LifeCareerPage(),
-        '/class8/arts_culture': (context) => const Class8ArtsCulturePage(),
+        '/class8/religion': (context) => const Class7IslamicStudiesPage(),
       },
     );
   }
