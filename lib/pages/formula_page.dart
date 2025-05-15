f0e684e15d82e337ffb008afdf435476062b0b50
pi-dev-0 <pimathematics1@gmail.com>
2025-05-15 12:04:07 +0600

    Fix context usage in PDF navigation to avoid potential errors

--- lib/pages/formula_page.dart
+++ lib/pages/formula_page.dart
@@ -386,6 +386,7 @@ class _FormulaPageState extends State<FormulaPage> {
                 final filePath =
                     '${directory.path}/${title.replaceAll(' ', '_')}.pdf';
                 Navigator.push(
+                  // ignore: use_build_context_synchronously
                   context,
                   MaterialPageRoute(
                     builder: (context) => PDFViewerPage(
